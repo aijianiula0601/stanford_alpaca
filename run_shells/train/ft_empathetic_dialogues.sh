f73e383900aaeb619adbfbe56cf02aef4d7e4319
aijianiula0601@gmail.com <348348jia>
2023-04-03 17:53:44 +0800

    update

--- run_shells/train/ft_empathetic_dialogues.sh
+++ run_shells/train/ft_empathetic_dialogues.sh
@@ -32,14 +32,14 @@ torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/empath
     --model_name_or_path "${pretrained_model_output_dir}" \
     --data_path ${data_json} \
     --output_dir ${your_output_dir} \
-    --num_train_epochs 5 \
+    --num_train_epochs 8 \
     --per_device_train_batch_size 64 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 1000 \
-    --save_total_limit 2 \
+    --save_steps 50 \
+    --save_total_limit 1000 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
