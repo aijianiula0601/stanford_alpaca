7ead8665baf2d12838109a3dca6d1ed9c5aa7309
aijianiula0601@gmail.com <348348jia>
2023-04-03 20:37:18 +0800

    update

---
+++ run_shells/train/ft_llama7B_52k_v1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -ex
+
+curdir=$(pwd)
+echo "curdir:$curdir"
+cd "$curdir" || exit
+
+cd ../../
+
+
+
+#----------------------------------------------------------
+# 在v100中训练，需要去掉：
+#   --bf16 True
+#   --tf32 True
+# 这两个参数，这两个参数是在A100机器上训练的。
+# 经历对齐 https://github.com/pointnetwork/point-alpaca 来训练
+#----------------------------------------------------------
+
+your_random_port=11223
+#your_path_to_hf_converted_llama_ckpt_and_tokenizer="decapoda-research/llama-7b-hf"
+#your_path_to_hf_converted_llama_ckpt_and_tokenizer="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/ft_52k/llama-7b-hf"
+
+your_path_to_hf_converted_llama_ckpt_and_tokenizer="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/pretrain_models/llama/new_llama_7b"
+
+your_output_dir="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/ft_52k/train_output_align_point_alpaca"
+
+#data_json="./alpaca_data.json"
+data_json="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/ft_52k/alpaca_data_cleaned.json"
+
+torchrun --nproc_per_node=8 --master_port=${your_random_port} train.py \
+    --model_name_or_path "${your_path_to_hf_converted_llama_ckpt_and_tokenizer}" \
+    --data_path ${data_json} \
+    --output_dir ${your_output_dir} \
+    --num_train_epochs 10 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 250 \
+    --save_total_limit 1000 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "tensorboard" \
+    --gradient_checkpointing True \
+    --deepspeed ${curdir}/deepspeed_config.json \
+    --fp16 True
