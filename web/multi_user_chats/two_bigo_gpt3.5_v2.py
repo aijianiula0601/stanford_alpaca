aa8e71c6e929792a8aa20537aeed88bc4f1b6ac5
aijianiula <aijianiula0601@gmail.com>
2023-04-17 23:25:52 +0800

    update

--- web/multi_user_chats/two_bigo_gpt3.5_v2.py
+++ web/multi_user_chats/two_bigo_gpt3.5_v2.py
@@ -16,35 +16,24 @@ gpt_config = {'engine': 'bigo-gpt35',
 
 def chat_with_chatgpt(prompt, role_a_name, role_b_name):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        engine='bigo-gpt35',
         prompt=prompt,
         temperature=0.9,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
-        stop=[f" {role_a_name}:", f" {role_b_name}:"]
+        stop=[f"{role_a_name}:", f"{role_b_name}:"]
     )
-    response_text = response.choices[0].message.content
-    return response_text
+    return response.choices[0].text
 
 
-def get_input_api_data(background, history=[]):
-    data_list = [{'role': 'system', 'content': background}]
-    for i, h in enumerate(history):
-        if i % 2 == 0:
-            data_list.append({"role": 'user', "content": h})
-        else:
-            data_list.append({'role': 'assistant', 'content': h})
-
-    return data_list
-
-
-background_post_text = "The following is a conversation with {role_name_a} and {role_name_a} "
+background_post_text = "The following is a conversation with {role_name_a} and {role_name_a}.\n\n"
 
 
 def get_background(background, role_name_a, role_name_b):
-    return background.format_map({"role_name_a": role_name_a, "role_name_b": role_name_b})
+    return background + "\n\n" + background_post_text.format_map(
+        {"role_name_a": role_name_a, "role_name_b": role_name_b})
 
 
 if __name__ == '__main__':
@@ -62,31 +51,12 @@ if __name__ == '__main__':
     print(f"{role_A_name}:", start_question)
 
     role_a_question = start_question
-    history = [start_question]
-
-    # role_b_input_api_data = get_input_api_data(get_background(backgroundB, role_B_name, role_A_name),
-    #                                            history=[])
-    # role_b_question = chat_with_chatgpt(role_b_input_api_data, user=role_B_name)
-    # print("role_b_question:", role_b_question)
-
+    prompt = get_background(backgroundA, role_A_name,
+                            role_B_name) + f"{role_A_name}:{role_a_question}\n{role_B_name}:"
     while True:
-        role_b_input_api_data = get_input_api_data(get_background(backgroundB, role_B_name, role_A_name),
-                                                   history=history)
-
-        print("---role_b_input_api_data:", role_b_input_api_data)
-        role_b_question = chat_with_chatgpt(role_b_input_api_data, user=role_A_name)
+        role_b_question = chat_with_chatgpt(prompt, role_A_name, role_B_name)
         print(f"{role_B_name}:", role_b_question)
-        history.append(role_b_question)
-        time.sleep(3)
-
-        print("=" * 20)
-
-        role_a_input_api_data = get_input_api_data(get_background(backgroundA, role_A_name, role_B_name),
-                                                   history=history)
-        print("---role_a_input_api_data:", role_a_input_api_data)
-        role_a_question = chat_with_chatgpt(role_a_question, user=role_B_name)
+        prompt += f"{role_b_question}\n{role_A_name}:"
+        role_a_question = chat_with_chatgpt(prompt, role_A_name, role_B_name)
         print(f"{role_A_name}:", role_a_question)
-        history.append(role_a_question)
-        print("-" * 100)
-        print()
-        print()
+        prompt += f"{role_a_question}\n{role_B_name}:"
