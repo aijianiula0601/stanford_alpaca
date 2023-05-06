7889fea0717da20e413eef3a4baa619481a9b299
aijianiula0601@gmail.com <348348jia>
2023-05-06 16:17:04 +0800

    update

--- dataset/rw_to_qa_dataset.py
+++ dataset/rw_to_qa_dataset.py
@@ -68,15 +68,13 @@ def process_f(org_f, columns=['prompt', 'response'], if_check=True):
     return all_dialogue_data
 
 
-# base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rm"
-#
-# test_f = f"{base_dir}/test-00000-of-00001-8c7c51afc6d45980.parquet"
-# train_f = f"{base_dir}/train-00000-of-00001-2a1df75c6bce91ab.parquet"
-#
-# rm_static_train_data = process_f(train_f)
-# random.shuffle(rm_static_train_data)
-# print("-----:", len(rm_static_train_data))
-#
+base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rm/rm-static"
+
+test_f = f"{base_dir}/test-00000-of-00001-8c7c51afc6d45980.parquet"
+train_f = f"{base_dir}/train-00000-of-00001-2a1df75c6bce91ab.parquet"
+
+rm_static_train_data = process_f(train_f)
+
 # test_data = process_f(test_f)
 # random.shuffle(test_data)
 # print(json.dumps(test_data))
@@ -85,35 +83,51 @@ def process_f(org_f, columns=['prompt', 'response'], if_check=True):
 # full-hh-rlhf
 # ------------------------------------------------------------
 
-# base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rl/full-hh-rlhf"
-#
-# test_f = f"{base_dir}/test-00000-of-00001-ec71e9262143a91c.parquet"
-# train_f = f"{base_dir}/train-00000-of-00001-8349d0765e6718df.parquet"
-#
-# full_hh_rlhf_train_data = process_f(train_f)
-# random.shuffle(full_hh_rlhf_train_data)
+base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rl/full-hh-rlhf"
+
+test_f = f"{base_dir}/test-00000-of-00001-ec71e9262143a91c.parquet"
+train_f = f"{base_dir}/train-00000-of-00001-8349d0765e6718df.parquet"
+
+full_hh_rlhf_train_data = process_f(train_f)
 # print(json.dumps(full_hh_rlhf_train_data))
 
 # ------------------------------------------------------------
 # synthetic-instruct-gptj-pairwise
 # ------------------------------------------------------------
 
-# base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rm/synthetic-instruct-gptj-pairwise"
-# org_f = f"{base_dir}/train-00000-of-00001-1e5d57b93c448e7a.parquet"
-#
-# synthetic_instruct_gptj_pairwise_data = []
-# data = pq.read_table(org_f, columns=['prompt', 'chosen'])
-# for i, row in data.to_pandas().iterrows():
-#     synthetic_instruct_gptj_pairwise_data.append(
-#         [{FROM_KEY: HUMAN_NAME, VALUE_KEY: row['prompt']}, {FROM_KEY: ASSISTANT_NAME, VALUE_KEY: row['chosen']}])
+base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rm/synthetic-instruct-gptj-pairwise"
+org_f = f"{base_dir}/train-00000-of-00001-1e5d57b93c448e7a.parquet"
+
+synthetic_instruct_gptj_pairwise_data = []
+data = pq.read_table(org_f, columns=['prompt', 'chosen'])
+for i, row in data.to_pandas().iterrows():
+    synthetic_instruct_gptj_pairwise_data.append(
+        [{FROM_KEY: HUMAN_NAME, VALUE_KEY: row['prompt']}, {FROM_KEY: ASSISTANT_NAME, VALUE_KEY: row['chosen']}])
 # print(json.dumps(synthetic_instruct_gptj_pairwise_data))
 
 # ------------------------------------------------------------
 # yitingxie/rlhf-reward-datasets
 # ------------------------------------------------------------
 
-# base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rl/rlhf-reward-datasets"
-# train_f = f"{base_dir}/train-00000-of-00001-2ea3039ca4da89f8.parquet"
-# rlhf_reward_datasets_data = process_f(train_f, columns=['prompt', 'chosen'])
-# random.shuffle(rlhf_reward_datasets_data)
+base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rl/rlhf-reward-datasets"
+train_f = f"{base_dir}/train-00000-of-00001-2ea3039ca4da89f8.parquet"
+rlhf_reward_datasets_data = process_f(train_f, columns=['prompt', 'chosen'])
 # print(json.dumps(rlhf_reward_datasets_data))
+
+# ------------------------------------------------------------
+# combine all
+# ------------------------------------------------------------
+base_dir = "/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/multi_turns_conversation_nomask"
+save_f = f"{base_dir}/add_deepspeedchat_example_dataset/train.json"
+deepspeedchat_example_dataset_data = rm_static_train_data + full_hh_rlhf_train_data + synthetic_instruct_gptj_pairwise_data + rlhf_reward_datasets_data
+
+org_multi_turns_f = f"{base_dir}/multi_dataset_qas.json"
+
+org_multi_turns_data = json.load(open(org_multi_turns_f))
+
+all_data = deepspeedchat_example_dataset_data + org_multi_turns_data
+random.shuffle(all_data)
+
+print(f"examples:{len(all_data)}")
+json.dump(all_data, fp=open(save_f, 'w'))
+print(f"save to:{save_f}")
