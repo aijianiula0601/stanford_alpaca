ac825f2a0a743f8987783755ca9c7f104e44d93d
aijianiula0601@gmail.com <348348jia>
2023-05-06 14:15:38 +0800

    update

--- dataset/rw_to_qa_dataset.py
+++ dataset/rw_to_qa_dataset.py
@@ -1,4 +1,7 @@
 import json
+import re
+import sys
+import random
 from pandas import read_parquet
 import pyarrow.parquet as pq
 
@@ -11,60 +14,47 @@ import pyarrow.parquet as pq
 # Dahoas/rm-static
 # url:https://huggingface.co/datasets/Dahoas/rm-static
 # ------------------------------------------------------------
-
 HUMAN_NAME = "Human"
 ASSISTANT_NAME = "Assistant"
+FROM_KEY = "from"
+VALUE_KEY = "value"
+
+
+def process_f(org_f: str):
+    data = pq.read_table(org_f, columns=['prompt', 'response'])
+
+    all_dialogue_data = []
+    for i, row in data.to_pandas().iterrows():
+        line = row['prompt'].strip() + " " + row['response']
+        cleaned_line = []
+        for d in line.split("\n"):
+            if d.strip("\n").strip() == "":
+                continue
+            cleaned_line.append(d)
+
+        cur_qas_split_list = re.split('(Human:|Assistant:)', '\n'.join(cleaned_line).strip())[1:]
+        assert len(cur_qas_split_list) % 2 == 0, f"error qa line:{cur_qas_split_list}"
+
+        cur_qas_list = []
+        for qa_group in [cur_qas_split_list[i:i + 2] for i in range(0, len(cur_qas_split_list), 2)]:
+            if qa_group[0] == HUMAN_NAME + ":":
+                cur_qas_list.append({FROM_KEY: HUMAN_NAME, VALUE_KEY: qa_group[1].strip()})
+            elif qa_group[0] == ASSISTANT_NAME + ":":
+                cur_qas_list.append({FROM_KEY: ASSISTANT_NAME, VALUE_KEY: qa_group[1].strip()})
+            else:
+                raise Exception(f"Error qa:{cur_qas_split_list}")
+
+        assert len(cur_qas_split_list) % 2 == 0, f"Error cur_qa_list:{cur_qas_list}"
+        all_dialogue_data.append(cur_qas_list)
+
+    return all_dialogue_data
+
 
 base_dir = "/mnt/cephfs/hjh/common_dataset/nlp/rm"
 
 train_f = f"{base_dir}/train-00000-of-00001-2a1df75c6bce91ab.parquet"
 test_f = f"{base_dir}/test-00000-of-00001-8c7c51afc6d45980.parquet"
 
-data = pq.read_table(test_f, columns=['prompt', 'response'])
-print("data line number:", len(data))
-
-print('-' * 100)
-print('-' * 100)
-print(data.to_pandas())
-
-all_dialogue_data = []
-for i, row in data.to_pandas().iterrows():
-    line = row['prompt'].strip() + " " + row['response']
-
-
-    cur_turns_list = []
-    pre_qa = ''
-    for d in line.split("\n"):
-        if d.strip("\n") == "":
-            continue
-        if d.startswith(HUMAN_NAME + ":") or d.startswith(ASSISTANT_NAME + ":"):
-            if pre_qa == "":
-                continue
-            # -----------
-            # 一个问答
-            # -----------
-            if pre_qa.startswith(ASSISTANT_NAME + ":"):
-                pre_qa_dic = {"from": HUMAN_NAME, "value": pre_qa}
-            else:
-                pre_qa_dic = {"from": ASSISTANT_NAME, "value": pre_qa}
-            cur_turns_list.append(pre_qa_dic)
-            pre_qa = d
-        else:
-            pre_qa += d
-
-    # -----------
-    # 最后一个
-    # -----------
-    if pre_qa.startswith(ASSISTANT_NAME + ":"):
-        pre_qa_dic = {"from": HUMAN_NAME, "value": pre_qa}
-    else:
-        pre_qa_dic = {"from": ASSISTANT_NAME, "value": pre_qa}
-    cur_turns_list.append(pre_qa_dic)
-
-    # -----------
-    # 对话加入
-    # -----------
-    print(cur_turns_list)
-    if len(cur_turns_list) > 1:
-        all_dialogue_data.append(cur_turns_list)
-    print("-" * 100)
+test_data = process_f(train_f)
+random.shuffle(test_data)
+print(json.dumps(test_data))
