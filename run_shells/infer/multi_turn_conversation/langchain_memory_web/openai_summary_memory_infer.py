1350a621d39aff10abe603baf95ec5072ab06ebf
aijianiula <aijianiula0601@gmail.com>
2023-05-14 07:28:31 +0800

    update

--- run_shells/infer/multi_turn_conversation/langchain_memory_web/openai_summary_memory_infer.py
+++ run_shells/infer/multi_turn_conversation/langchain_memory_web/openai_summary_memory_infer.py
@@ -52,45 +52,57 @@ class LLamaLLM(LLM, ABC):
         return text_respond.replace("#", "").strip()
 
 
+class LLamaMemoryInfer:
+    def __init__(self, background: str, role_dict: dict, max_token_limit: int = 40):
+        assert 'user' in role_dict and 'assistant' in role_dict
+        mess_dic = {"background": background,
+                    "role_a": role_dict['user'],
+                    "role_b": role_dict['assistant']}
+        header_text = PROMPT_DICT['conversion'].format_map(mess_dic)
+        template = header_text + """
+                    Summary of conversation:
+                    {history}
+                    Current conversation:
+                    {human_name}: {input}
+                    {bot_name}:""".format_map(
+            {'history': '{history}',
+             'input': '{input}',
+             'human_name': role_dict['user'],
+             'bot_name': role_dict['assistant']})
+        PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+
+        llm = LLamaLLM(role_dict=role_dict)
+        self.summary_memory = ConversationSummaryBufferMemory(llm=OpenAI(), max_token_limit=max_token_limit,
+                                                              input_key="input",
+                                                              human_prefix=role_dict['user'],
+                                                              ai_prefix=role_dict['assistant'])
+
+        self.conversation = ConversationChain(
+            llm=llm,
+            verbose=True,
+            memory=self.summary_memory,
+            prompt=PROMPT
+        )
+
+    def infer(self, user_input):
+        return self.conversation.predict(input=user_input)
+
+    def clear_memory(self):
+        self.summary_memory.clear()
+
+
 if __name__ == '__main__':
     role_dict = {'user': 'Emily', 'assistant': 'Audrey'}
     background = "Audrey is a 26-year-old entrepreneur who knows firsthand the challenges that come with dating in today's world. As someone who has experienced the ups and downs of relationships, Audrey wants to remind everyone that they are worthy of love and respect, no matter what they're looking for. She wishes everyone the best of luck in their search for companionship, be it sex, love, or friendship.When Audrey isn't busy with her entrepreneurial ventures, she enjoys traveling, hiking, and practicing yoga. In fact, she's planning to spend the next few weeks exploring India and the Himalayas, specifically Leh, Jammu & Kashmir, Manali, Dharam Rishikesh, and other areas. Audrey is always open to travel tips and recommendations, so if you have any, be sure to let her know! When it comes to her lifestyle, she wants a pet, but only on special occasions. She is a non-smoker who exercises every day and follows a vegetarian diet.Aside from her love of travel and adventure, Audrey is passionate about art, entrepreneurship, meditation, singing, and personal growth. She is always seeking new ways to learn and improve herself, both professionally and personally. Now you reply as Audrey."
-    mess_dic = {"background": background,
-                "role_a": role_dict['user'],
-                "role_b": role_dict['assistant']}
-    header_text = PROMPT_DICT['conversion'].format_map(mess_dic)
 
-    template = header_text + """
-    Summary of conversation:
-    {history}
-    Current conversation:
-    {human_name}: {input}
-    {bot_name}:""".format_map(
-        {'history': '{history}',
-         'input': '{input}',
-         'human_name': role_dict['user'],
-         'bot_name': role_dict['assistant']})
-    PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+    llama_memory_infer = LLamaMemoryInfer(background, role_dict)
 
-    llm = LLamaLLM(role_dict=role_dict)
-
-    # summary_memory = ConversationSummaryMemory(llm=OpenAI(), input_key="input", human_prefix="Emily", ai_prefix="Audrey")
-    summary_memory = ConversationSummaryBufferMemory(llm=OpenAI(), max_token_limit=40, input_key="input",
-                                                     human_prefix=role_dict['user'], ai_prefix=role_dict['assistant'])
-
-    conversation = ConversationChain(
-        llm=llm,
-        verbose=True,
-        memory=summary_memory,
-        prompt=PROMPT
-    )
-
-    print(conversation.predict(input="Hi Audrey!"))
+    print(llama_memory_infer.infer(user_input="Hi Audrey!"))
     print('-' * 100)
-    print(conversation.predict(input="I am so sad, Audrey!"))
+    print(llama_memory_infer.infer(user_input="I am so sad, Audrey!"))
     print('-' * 100)
-    print(conversation.predict(input="I can't travel to India."))
+    print(llama_memory_infer.infer(user_input="I can't travel to India."))
     print('-' * 100)
-    print(conversation.predict(input="Are you going on a trip?"))
+    print(llama_memory_infer.infer(user_input="Are you going on a trip?"))
     print('-' * 100)
-    print(conversation.predict(input="When are you going to go? How can I get there?"))
+    print(llama_memory_infer.infer(user_input="When are you going to go? How can I get there?"))
