7c71a4a80cf4143419ae372817848fc8881c9daa
aijianiula0601@gmail.com <348348jia>
2023-05-19 17:44:56 +0800

    update

--- run_shells/train/bigolive_soda/dataset_process.py
+++ run_shells/train/bigolive_soda/dataset_process.py
@@ -43,7 +43,8 @@ def process_data(qas_data, save_f, end_string_list=[], header_key=None):
                         qa[header_key] = qa[header_key].rstrip(end_string).strip()
 
                     qa[BACKGROUND] = qa[header_key]
-                    del qa[header_key]
+                    if BACKGROUND != header_key:
+                        del qa[header_key]
 
         if flag:
             continue
@@ -53,6 +54,61 @@ def process_data(qas_data, save_f, end_string_list=[], header_key=None):
     print(f"all_n:{all_n},multi_speaker_skip_n:{multi_speaker_skip_n},not_head_skip_n:{not_head_skip_n}")
     json.dump(new_data, open(save_f, 'w'))
     print(f"save to:{save_f}")
+
+    return new_data
+
+
+# -----------------
+# 转换为qa格式
+# -----------------
+HUMAN_NAME_KEY = "human_name"
+BOT_NAME_KEY = "bot_name"
+BACKGROUND_KEY = "background"
+QAS_KEY = "qas"
+QUESTION_KEY = "question"
+ANSWER_KEY = "answer"
+DATASET_KEY = "dataset_name"
+
+
+def trans2qa(all_data, dataset_name):
+    new_data = []
+    skip_n = 0
+    for turns_data in all_data:
+        background = None
+        qas = {}
+        human_name = turns_data[0]['from']
+        bot_name = turns_data[1]['from']
+        turn_i = 0
+
+        try:
+            for i, td in enumerate(turns_data):
+                if i == 0:
+                    background = td['background']
+                if (i + 1) % 2 == 1:
+                    assert human_name == td['from'], f"error qas:{json.dumps(turns_data)}"
+                    qas[f"turn_{turn_i}"] = {QUESTION_KEY: td['value']}
+                else:
+                    assert bot_name == td['from']
+                    qas[f"turn_{turn_i}"][ANSWER_KEY] = td['value'].strip()
+                    turn_i += 1
+        except Exception as e:
+            skip_n += 1
+            continue
+
+        turn_n = len(qas)
+        if QUESTION_KEY not in qas[f"turn_{turn_n - 1}"] or ANSWER_KEY not in qas[f"turn_{turn_n - 1}"]:
+            qas.pop(f"turn_{turn_n - 1}")
+
+        if len(qas) < 1:
+            continue
+
+        assert background is not None
+        new_data.append(
+            {BACKGROUND_KEY: background,
+             DATASET_KEY: dataset_name, HUMAN_NAME_KEY: human_name, BOT_NAME_KEY: bot_name,
+             QAS_KEY: qas})
+
+    print(f"----skip qas nums:{skip_n}")
 
     return new_data
 
@@ -68,6 +124,8 @@ end_string_list = ["They start a conversation:", "The conversation started"]
 
 new_sex_data_list = process_data(data_list, save_f, end_string_list, header_key='handPrompt')
 
+new_sex_data_list = trans2qa(new_sex_data_list, dataset_name="sex")
+
 # -----------------
 # bigolive
 # -----------------
@@ -80,6 +138,8 @@ data_list = json.load(open(f))
 
 new_biglive_data_list = process_data(data_list, save_f, header_key="background")
 
+new_biglive_data_list = trans2qa(new_biglive_data_list, dataset_name="bigolive")
+
 # -----------------
 # soda
 # -----------------
@@ -89,6 +149,7 @@ save_f = f"{base_dir}/sota.json"
 data_list = json.load(open(f))
 
 new_soda_data_list = process_data(data_list, save_f, header_key="narrative")
+new_soda_data_list = trans2qa(new_soda_data_list, dataset_name="soda")
 
 # -----------------
 # merge
