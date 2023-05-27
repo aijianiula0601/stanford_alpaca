7175fc203f153436604a436eeb0774bd6e874df2
aijianiula0601@gmail.com <348348jia>
2023-05-27 10:40:43 +0800

    update

--- dataset/data_utils.py
+++ dataset/data_utils.py
@@ -14,6 +14,17 @@ DATASET_KEY = "dataset_name"
 # 定义各个数据集的prompt
 # ---------------------------------------------------------------------------------------------
 
+#  qa数据中一个example为：
+#   {
+#         "background":"---",
+#         "human_name":"a",
+#         "bot_name":"b",
+#         "qas":{
+#             "turn_0":{"question":"---","answer":"---"},
+#             ...
+#             "turn_n":{"question":"---","answer":"---"}
+#         }
+#   }
 
 def sota_prompt(human_name, bot_name, background):
     return f"Here is a conversation between {human_name} and {bot_name} related to the description below. {background}\n\n"
@@ -28,10 +39,32 @@ def persona_chat_prompt(human_name, bot_name, background):
 
 
 def empathetic_dialogues_prompt(human_name, bot_name, background):
-    return f"Here is a conversation between {human_name} and {bot_name}.\n"
+    return f"Here is a conversation between {human_name} and {bot_name}.\n\n"
 
 
 def stanford_52k_prompt(human_name, bot_name, background=""):
+    """
+    PROMPT_DICT = {
+        "prompt_input": (
+            "Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+        ),
+        "prompt_no_input": (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Response:"
+        ),
+    }
+    原型是这样的，所以在处理为qa格式的时候，human_name="Instruction"  bot_name="Response", background的值为input的值。
+
+    这里需要特殊处理
+    1. background为空，也就是input==""时候
+        qa数据集中的某个question的值应该连着input，比如："{instruction} ### Input: {input}" 这样才对。
+    2. background不为空，也就是没有inpput=="~"时候
+        qa数据集中的某个question的值应该为Instruction的值，比如："{instruction}" 这样才对。
+    """
+
     if background == "":
         return "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n"
     else:
