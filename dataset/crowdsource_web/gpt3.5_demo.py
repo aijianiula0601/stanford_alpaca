89ebf81013438654c6514f894e15b586b1d06be7
aijianiula0601@gmail.com <348348jia>
2023-06-07 17:11:07 +0800

    update

---
+++ dataset/crowdsource_web/gpt3.5_demo.py
@@ -0,0 +1,31 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+
+openai.api_type = "azure"
+openai.api_base = "https://bigo-chatgpt-9.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+# key1: 19ea901e8e10475da1bb0537abf8e5a4
+# key2: 548e5c0c2aff453e932948927a27bde6
+openai.api_key = "548e5c0c2aff453e932948927a27bde6"
+
+
+# role : system|user|assistant
+gpt_config = {'engine': 'gpt-35-turbo',
+              'role': 'user',
+              }
+
+
+response = openai.ChatCompletion.create(
+    engine=gpt_config['engine'],
+    messages=[
+        {"role": "system",
+         "content": "Let's play a role game. Alice and Kervin are classmate. You are Kervin. I am Alice."},
+        {"role": "user", "content": "hi, Kervin"},
+        {"role": "assistant", "content": "I am fine, how about you?"},
+        {"role": "user", "content": "Where are you?"},
+        {"role": "assistant", "content": "I am at home, what about you?"},
+        {"role": "user", "content": "Let's go to the library. Will you come with me?"}
+    ]
+)
+
+print(response)
--- dataset/crowdsource_web/two_bigo_gpt35.py
+++ dataset/crowdsource_web/two_bigo_gpt35.py
@@ -11,9 +11,9 @@ import openai
 openai.api_type = "azure"
 openai.api_base = "https://bigo-chatgpt-9.openai.azure.com/"
 openai.api_version = "2023-03-15-preview"
-#----------------
-#非线上的key
-#----------------
+# ----------------
+# 非线上的key
+# ----------------
 # key1: 19ea901e8e10475da1bb0537abf8e5a4
 # key2: 548e5c0c2aff453e932948927a27bde6
 openai.api_base = "https://bigo-chatgpt-9.openai.azure.com/"
@@ -24,20 +24,9 @@ gpt_config = {'engine': 'gpt-35-turbo',
               }
 
 
-# messages = [
-#     {"role": "system",
-#      "content": "Let's play a role game. Alice and Kervin are classmate. You are Kervin. I am Alice."},
-#     {"role": "user", "content": "hi, Kervin"},
-#     {"role": "assistant", "content": "I am fine, how about you?"},
-#     {"role": "user", "content": "Where are you?"},
-#     {"role": "assistant", "content": "I am at home, what about you?"},
-#     {"role": "user", "content": "Let's go to the library. Will you come with me?"}
-# ]
-
-
 def chat_with_chatgpt(messages, selected_temp=0.95):
     response = openai.ChatCompletion.create(
-        engine='gpt-35-turbo',
+        engine=gpt_config['engine'],
         temperature=selected_temp,
         messages=messages
     )
