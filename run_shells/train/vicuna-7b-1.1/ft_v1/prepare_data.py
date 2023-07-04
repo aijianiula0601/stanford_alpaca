2292a0f130f8d5a08f9f50f9dd900331ca8d90b3
aijianiula0601@gmail.com <348348jia>
2023-07-04 15:54:30 +0800

    update

--- run_shells/train/vicuna-7b-1.1/ft_v1/prepare_data.py
+++ run_shells/train/vicuna-7b-1.1/ft_v1/prepare_data.py
@@ -68,35 +68,15 @@ print("-" * 100)
 # ---------------
 all_data_list = gpt35sex_data_list + sexy_data_list
 
-# 过来有空回复的对话
-new_data_list = []
-all_n = 0
-skip_n = 0
-for example in tqdm(all_data_list):
-    all_n += 1
-    try:
-        for i in range(len(example['qas'])):
-            qa = example["qas"][f"turn_{i}"]
-            question = qa['question'].strip()
-            answer = qa['answer'].strip()
-            assert question != "" and answer != "", f"empty, question:{question}\n answer:{answer}\n"
-
-            new_data_list.append(example)
-    except Exception as e:
-        print(e)
-        print(f"example:{json.dumps(example)}")
-        skip_n += 1
-        print("-" * 100)
-
-print(f"all_data:{len(new_data_list)},skip empty qa:{skip_n}")
+print(f"all_data:{len(all_data_list)}")
 
 save_dir = sys.argv[1]
 os.system(f"mkdir -p {save_dir}")
 save_f = f"{save_dir}/train_data.json"
 save_debug_f = f"{save_dir}/debug_data.json"
 
-json.dump(new_data_list, open(save_f, 'w'))
-json.dump(new_data_list[:200], open(save_debug_f, 'w'))
+json.dump(all_data_list, open(save_f, 'w'))
+json.dump(all_data_list[:200], open(save_debug_f, 'w'))
 
 print(f"save to:{save_f}")
 print(f"save to:{save_debug_f}")
