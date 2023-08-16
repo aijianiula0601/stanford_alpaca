6e2e11b97d88400c54f21f9be88275048d81f8b5
aijianiula0601@gmail.com <348348jia>
2023-08-16 17:27:40 +0800

    update

--- run_shells/train/vicuna7b_11/ft_v15/v5/prepare_data.py
+++ run_shells/train/vicuna7b_11/ft_v15/v5/prepare_data.py
@@ -10,42 +10,89 @@ sys.path.append(pdj)
 
 from dataset.data_utils import *
 
+keywords_list = [
+    "thanks for asking.",
+    "How are you today?",
+    "How are you doing?",
+    "How about you?",
+    "How's it going?",
+    "What's up?"
+    "What have you been up to lately?",
+    "What have you been up to today?",
+    "do you want to talk about?",
+    "nice to meet you!",
+    "That's great to hear!",
+    "hobbies or interests",
+    "what are your interests?",
+    "How's your day going so far?",
+]
 
-# -------------------------------------------------------
-# 对于多轮的对话，随机采样一个轮次，最后训练时候是训练最后一个answer
-# -------------------------------------------------------
+base_dir = "/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/vicuna-7b/ft2_v15/v3"
+org_f = f"{base_dir}/train_data.txt"
 
-def sample_qas_and_mask(example):
-    qas = example[QAS_KEY]
-    assert len(qas) > 1, f"error length of qas:{len(qas)}"
+keywords_num_dic = {}
+other_example_list = []
+bigolive_other_example_list = []
+bigo_n = 0
+with open(org_f) as fr:
+    for line in tqdm(fr.readlines()):
+        example = json.loads(line)
+        # 只检查bigolive数据,之前的数据bigolive只有一轮
+        if example[DATASET_KEY] == BIGOLIVE_ONLINE_CHAT_DATASET_NAME:
+            bigo_n += 1
+            qa = example[QAS_KEY][f"{TURN_KEY}_0"]
+            k_flag = False
+            for k in keywords_list:
+                if k in qa[ANSWER_KEY]:
+                    k_flag = True
+                    break
+            if k_flag:
+                if k not in keywords_num_dic:
+                    keywords_num_dic[k] = []
+                keywords_num_dic[k].append(example)
+                continue
 
-    random_turn_i = random.randint(1, len(qas) - 1)
-    for i in range(random_turn_i, len(qas)):
-        del example[QAS_KEY][f"{TURN_KEY}_{i}"]
+            bigolive_other_example_list.append(example)
 
-    # 用户先提问的，mask除了最后一个answer的所有字符
-    if example[MASK_QUESTION_KEY]:
-        example[MASK_EXCEPT_LAST_ANSWER] = True
-        example[MASK_EXCEPT_LAST_QUESTION_ANSWER] = False
-    else:
-        # 如果原始的数据不能mask question，那么只能mask最后一个qa前所有字符
-        example[MASK_EXCEPT_LAST_ANSWER] = False
-        example[MASK_EXCEPT_LAST_QUESTION_ANSWER] = True
+        # 非bigolive数据
+        else:
+            other_example_list.append(example)
 
+print(f"-------bigo_n:{bigo_n}")
+print("-" * 100)
+k_n = 0
+for k in keywords_num_dic:
+    print(f"keyword:{k},n:{len(keywords_num_dic[k])}")
+    k_n += len(keywords_num_dic[k])
+print(f"k_n:{k_n}")
+print(f"bigolive_other_example_list:{len(bigolive_other_example_list)},bigo_n:{bigo_n}")
 
-if __name__ == '__main__':
-    base_dir = "/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/vicuna-7b/ft2_v15/v3_v2"
-    org_f = f"{base_dir}/train_data.txt"
-    save_f = sys.argv[1]
+# -----------------------------------
+# 带有关键词的，只采样5个对话
+# -----------------------------------
 
-    with open(save_f, 'w', buffering=1) as fw:
-        with open(org_f) as fr:
-            for line in tqdm(fr.readlines()):
-                example = json.loads(line)
-                if len(example[QAS_KEY]) > 1:
-                    # 随机采样一定轮次
-                    sample_qas_and_mask(example)
+for k in keywords_num_dic:
+    keywords_num_dic[k] = random.sample(keywords_num_dic[k], k=min([5, len(keywords_num_dic[k])]))
 
-                fw.write(f"{json.dumps(example)}\n")
+# -----------------------------------
+# 结合到其他对话中
+# -----------------------------------
 
-    print(f"save to:{save_f}")
+all_example_list = []
+for k in keywords_num_dic:
+    for e in keywords_num_dic[k]:
+        all_example_list.append(e)
+
+random.shuffle(bigolive_other_example_list)
+all_example_list += other_example_list + bigolive_other_example_list[:4000]
+
+# -----------------------------------
+# 保存
+# -----------------------------------
+
+save_f = sys.argv[1]
+with open(save_f, 'w') as fw:
+    for e in all_example_list:
+        fw.write(f"{json.dumps(e)}\n")
+
+print(f"all_n:{len(all_example_list)},save to:{save_f}")
