8c6754e7f0730918280f8bde34259b12ef97377e
aijianiula <aijianiula0601@gmail.com>
2023-08-26 16:28:43 +0800

    update

--- run_shells/train/vicuna-13b/ft_v2/ft_vicuna13b_v2.sh
+++ run_shells/train/vicuna-13b/ft_v2/ft_vicuna13b_v2.sh
@@ -13,7 +13,7 @@ cd ../../../../
 your_random_port=11224
 
 base_dir="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/vicuna-13b/ft_v2"
-llama_ckpt_and_tokenizer='jeffwan/vicuna-13b'
+llama_ckpt_and_tokenizer='eachadea/vicuna-13b-1.1'
 output_dir="${base_dir}/ft_out"
 data_json="${base_dir}/train_data.txt"
 cache_dir="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/pretrain_models/hungging"
@@ -23,27 +23,29 @@ mkdir -p ${output_dir}
 
 if [ ! -f "${data_json}" ]; then
   echo "-------------------------prepare_data-----------------------------------------"
-  org_f='/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/vicuna-7b/ft2_v15/v6_v2/train_data.txt'
+  org_f='/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/vicuna-7b/ft2_v15/v6_v3/train_data.txt'
   ln -s ${org_f} ${data_json}
   echo "------------------------------------------------------------------------------"
 fi
 
-
+##----------------------
+## train
+##----------------------
 #CUDA_VISIBLE_DEVICES=0,1,2,3 \
 torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/vicuna-7b/train_mask_control_in_data.py \
     --model_name_or_path "${llama_ckpt_and_tokenizer}" \
     --data_path ${data_json} \
     --cache_dir ${cache_dir} \
     --output_dir ${output_dir} \
-    --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "epoch" \
     --save_on_each_node \
     --model_max_length 2048 \
-    --save_total_limit 30 \
+    --save_total_limit 10 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
@@ -54,7 +56,4 @@ torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/vicuna
     --deepspeed run_shells/train/deepspeed_config_zero2.json \
     --fp16 True \
     --process_name "vicuna-13b-ft_v2" \
-    --lazy_load \
-    --mask_head \
-    --mask_question
-
+    --lazy_load
