dfef92c55f2aeb24b0be30867b239d2625b5d6b3
aijianiula0601@gmail.com <348348jia>
2023-08-30 16:15:01 +0800

    update

--- run_shells/train/wizardlm7b/ft_v1/ft_wizardlm7b_deepspeed_v1.sh
+++ run_shells/train/wizardlm7b/ft_v1/ft_wizardlm7b_deepspeed_v1.sh
@@ -32,7 +32,7 @@ torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/llama2
     --output_dir ${output_dir} \
     --cache_dir ${cache_dir} \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 6 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
