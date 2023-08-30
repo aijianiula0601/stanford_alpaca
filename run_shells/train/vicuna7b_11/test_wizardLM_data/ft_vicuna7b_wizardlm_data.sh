a95d7ffe7057949429f8c3e6407d39082d67252f
aijianiula0601@gmail.com <348348jia>
2023-08-30 20:43:24 +0800

    update

--- run_shells/train/vicuna7b_11/test_wizardLM_data/ft_vicuna7b_wizardlm_data.sh
+++ run_shells/train/vicuna7b_11/test_wizardLM_data/ft_vicuna7b_wizardlm_data.sh
@@ -44,7 +44,7 @@ torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/vicuna
     --cache_dir ${cache_dir} \
     --output_dir ${output_dir} \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 3 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
