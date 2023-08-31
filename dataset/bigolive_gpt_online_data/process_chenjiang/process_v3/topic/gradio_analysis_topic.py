ff67eadd00ebb88e56f03380bc210d3212eb3ad4
aijianiula0601@gmail.com <348348jia>
2023-08-31 20:11:34 +0800

    添加topic的统计

--- dataset/bigolive_gpt_online_data/process_chenjiang/process_v3/topic/gradio_analysis_topic.py
+++ dataset/bigolive_gpt_online_data/process_chenjiang/process_v3/topic/gradio_analysis_topic.py
@@ -14,8 +14,8 @@ now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 # ----------------------------------------------
 
 
-base_dir = '/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/dataset/bigolive_gpt_online_data/chengjiang_data/v3/topic/votes'
-# base_dir = "/Users/jiahong/Downloads"
+# base_dir = '/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/dataset/bigolive_gpt_online_data/chengjiang_data/v3/topic/votes'
+base_dir = "/Users/jiahong/Downloads"
 vote_log_f = f"{base_dir}/vote_log.txt"
 user_vote_record_f = f"{base_dir}/user_vote_record.json"
 
@@ -54,27 +54,35 @@ def get_all_analysis_result():
         your_name_list = list(all_user_vote_info_dic.keys())
         finished_dialogues_list = []
         time_consume_list = []
+        topic_n_list = []
+        all_topic_n_list = set()
         for your_name in all_user_vote_info_dic:
             cur_time_consume = 0
             cur_fd = 0
+            cur_topic_set = set()
             for uid in all_user_vote_info_dic[your_name]:
                 if 'time_consume' in all_user_vote_info_dic[your_name][uid]:
                     cur_time_consume += all_user_vote_info_dic[your_name][uid]['time_consume']
                     cur_fd += 1
+                cur_topic_set.add(all_user_vote_info_dic[your_name][uid]['topic'])
+                all_topic_n_list.add(all_user_vote_info_dic[your_name][uid]['topic'])
 
             time_consume_list.append(round(cur_time_consume / 60, 2))
             finished_dialogues_list.append(cur_fd)
+            topic_n_list.append(len(cur_topic_set))
 
         your_name_n = len(your_name_list)
         finished_dialogues_sum = sum(finished_dialogues_list)
         time_consume_sum = sum(time_consume_list)
+        topic_n_sum = len(set(all_topic_n_list))
 
         your_name_list.insert(0, f"total users({your_name_n})")
         finished_dialogues_list.insert(0, f"total finished({finished_dialogues_sum})")
         time_consume_list.insert(0, f"total time consume({round(time_consume_sum, 2)})")
+        topic_n_list.insert(0, f"total topic({topic_n_sum})")
 
         return pd.DataFrame(
-            {'user name': your_name_list, 'finish dialogues': finished_dialogues_list,
+            {'user name': your_name_list, 'finish dialogues': finished_dialogues_list, 'finish topic': topic_n_list,
              "time_consume(hours)": time_consume_list})
     else:
         return None
@@ -98,30 +106,39 @@ def get_date_analysis(date_str: str, your_name: str):
                     end_date = all_user_vote_info_dic[name][uid_pair]['end_date']
                     if end_date == date_str.strip() or date_str == "" or date_str is None:
                         if name not in date_analysis_dic:
-                            date_analysis_dic[name] = {'uid_pair_n': 0, "time_consume": 0}
+                            date_analysis_dic[name] = {'uid_pair_n': 0, "time_consume": 0, 'topics': []}
 
                         date_analysis_dic[name]['uid_pair_n'] += 1
+                        date_analysis_dic[name]['topics'].append(all_user_vote_info_dic[name][uid_pair]['topic'])
                         date_analysis_dic[name]['time_consume'] += all_user_vote_info_dic[name][uid_pair][
                             'time_consume']
 
         name_list = []
         finished_dialogues_list = []
         time_consume_list = []
+        name_topic_n_list = []
         for name in date_analysis_dic:
             name_list.append(name)
             finished_dialogues_list.append(date_analysis_dic[name]['uid_pair_n'])
             time_consume_list.append(round(date_analysis_dic[name]['time_consume'] / 60, 2))
+            name_topic_n_list.append(len(set(date_analysis_dic[name]['topics'])))
 
         your_name_n = len(name_list)
         finished_dialogues_sum = sum(finished_dialogues_list)
         time_consume_sum = sum(time_consume_list)
+        topic_name_set = set()
+        for name in date_analysis_dic:
+            for topic in date_analysis_dic[name]['topics']:
+                topic_name_set.add(topic)
+        topic_n_sum = len(topic_name_set)
 
         name_list.insert(0, f"total users({your_name_n})")
         finished_dialogues_list.insert(0, f"total finished({finished_dialogues_sum})")
+        name_topic_n_list.insert(0, f"total topics({topic_n_sum})")
         time_consume_list.insert(0, f"total time consume({round(time_consume_sum, 2)})")
 
         return pd.DataFrame(
-            {'user name': name_list, 'finish dialogues': finished_dialogues_list,
+            {'user name': name_list, 'finish dialogues': finished_dialogues_list, "topic": name_topic_n_list,
              "time_consume(hours)": time_consume_list})
 
     return None
@@ -151,7 +168,6 @@ if __name__ == '__main__':
                                    value=None)
 
         analysis_table = gr.DataFrame(label="Evaluation results",
-                                      headers=['user name', "finish dialogues", "time_consume(hours)"],
                                       value=get_all_analysis_result)
 
         input_date.submit(analysis_table_submit, [input_date, your_name], analysis_table)
